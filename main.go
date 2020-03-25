6812e0a0296c49d6e513f61609a8174cdd8a9300
Robson <robsonr@seznam.cz>
2020-03-25 17:33:44 -0400

    another change

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang - upgate Robert"
+	title := "Jenkins X golang server - upgate robson"
 
 	from := ""
 	if r.URL != nil {
